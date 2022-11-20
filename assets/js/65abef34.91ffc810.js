6a11e8c716f7104b8cc9583357e76e1361d14d2e
xSwezan <101433236+xSwezan@users.noreply.github.com>
2022-11-20 11:55:52 +0100

    Built and published by Moonwave

---
+++ assets/js/65abef34.91ffc810.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[943],{7636:e=>{e.exports=JSON.parse('[{"type":"link","href":"/api/SuperGlobal","label":"\u200bSuper\u200bGlobal"},{"type":"link","href":"/api/Store","label":"\u200bStore"}]')}}]);
---
+++ assets/js/90f6ae56.d256d2c9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[33],{63891:e=>{e.exports=JSON.parse('{"sourceUrl":"https://github.com/xSwezan/SuperGlobal/blob/main","baseUrl":"/SuperGlobal/","classOrder":["SuperGlobal","Store"],"apiCategories":[]}')}}]);
